7f8e422629e99fff830dec8277b9d14302f1013b
Doncarlos67 <rule4real67@gmail.com>
2020-08-21 07:50:20 +0100

    new changes

--- alongkab/settings.py
+++ alongkab/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'h()8md8sq26^rsp=3s7r^*h*%vrz!ja&gquy@cxfz+(j^^bd52'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alongapi.herokuapp.com']
+ALLOWED_HOSTS = ['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+     'corsheaders',
     'djoser',
     'drivers',
     'passengers',
@@ -47,12 +50,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'alongkab.urls'
